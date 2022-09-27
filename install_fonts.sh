4d438e0b9da274662a729c0e34890fc1d6256002
tomhosker <tomdothosker@gmail.com>
2022-09-27 16:16:57 +0100

    Tightened up script for installing fonts.

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,13 +1,22 @@
 ### This script installs the fonts used by this package.
 
 # Local constants.
-PATH_TO_SOURCE="fonts"
+PATH_TO_SOURCE="$(dirname "$0")/fonts"
 PATH_TO_DEST="$HOME/.fonts"
 
 # Crash on the first error.
 set -e
 
-# Let's get cracking...
+#################
+# INSTALL FONTS #
+#################
+
+# Check fonts folder exists.
+if [ ! -d $PATH_TO_DEST ]; then
+    mkdir -p $PATH_TO_DEST
+fi
+
+# Copy font folders across.
 for directory in $PATH_TO_SOURCE/*/; do
-    cp -r $directory $PATH_TO_DEST
+    cp -ru $directory $PATH_TO_DEST
 done
